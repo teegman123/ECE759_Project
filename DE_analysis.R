766810e32838f8d86a4dee5043faecbf718bd809
Teague McCracken <temccrac@ncsu.edu>
2025-03-21 09:07:50 -0400

    updates

--- DE_analysis.R
+++ DE_analysis.R
@@ -1,8 +1,10 @@
 library(edgeR)
 
 # Load the data from csv file
-data <- read.csv('/Users/teaguemcc/Programs/git_clones/ECE759_Project/cellular_clarity/read_count_matrix.csv', row.names = 1)
+git_path = '/Users/teaguemcc/Programs/git_clones/ECE759_Project'
+data <- read.csv(git_path+'/cellular_clarity/read_count_matrix.csv', row.names = 1)
 data_matrix <- as.matrix(t(data)) # Transpose where columns are samples and rows are genes for edgeR
+sample_names <- read.csv(git_path+'/cellular_clarity/sample_names.csv')
 
 # Create DGEList Object for edgeR
 dge <- DGEList(counts = data_matrix)
